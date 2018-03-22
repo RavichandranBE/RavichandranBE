f3f98a7a8d82dbd77aa69204dfe82058607cb5c2
RavichandranBE <37319294+RavichandranBE@users.noreply.github.com>
2018-03-22 15:03:53 +0530

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,9 @@
+number=int(input("enter the length:"))
+list=[]
+for i in range(0,number):
+    newNo=int(input(''))
+    a=list.append(newNo)
+    print list
+list.sort()
+print("Sorted list is",list)
+pr
