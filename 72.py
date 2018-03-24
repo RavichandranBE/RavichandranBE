a59d6a92dca702f777f78fa284c352bd88c42014
RavichandranBE <37319294+RavichandranBE@users.noreply.github.com>
2018-03-24 13:24:04 +0530

    Create 72.py

---
+++ 72.py
@@ -0,0 +1,12 @@
+def hasvowel():
+	v=['a','e','i','o','u','A','E','I','O','U']
+	st=input()
+	for i in st:
+		if i in v:
+			return 'yes'
+	return 'no'
+try:
+    hasvowel()
+except:
+    print('invalid')
+    pr
