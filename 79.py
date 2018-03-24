8f575801e1839245e25f7b9a709afea914b42610
RavichandranBE <37319294+RavichandranBE@users.noreply.github.com>
2018-03-24 13:29:16 +0530

    Create 79.py

---
+++ 79.py
@@ -0,0 +1,16 @@
+import math
+def persqr():
+	n=int(input())
+	m=int(input())
+	m*=n
+	s=math.sqrt(m)
+	if s==int(s):
+		print('yes')
+	else :
+		print('no')
+try:
+	persqr()
+except:
+	print('invalid')
+
+p
