cb21c918621ad2608a3da1a245a308fc81db97d3
RavichandranBE <37319294+RavichandranBE@users.noreply.github.com>
2018-04-04 16:29:21 +0530

    Create 32.py

---
+++ player/32.py
@@ -0,0 +1,12 @@
+n=int(input("Enter N value:"))
+k=int(input("Enter K value:"))
+print("Enter your array:")
+a=[]
+for x in range(n):
+	m=int(input())
+	a.append(m)
+if k in a:
+	print("Yes")
+else:
+	print("No")
+  p
