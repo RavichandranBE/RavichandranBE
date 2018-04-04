04660ef999542c80db4862d771965e47091ac967
RavichandranBE <37319294+RavichandranBE@users.noreply.github.com>
2018-04-04 16:08:43 +0530

    Create 23.py

---
+++ player/23.py
@@ -0,0 +1,13 @@
+n=int(input("Enter the N value:"))
+k=int(input("Enter the N value:"))
+print("Enter your first array:")
+a=[]
+for x in range(n):
+	m=int(input())
+	a.append(m)
+print("Enter K array:")
+for x in range(k):
+	m=int(input())
+	a.append(m)
+	print(max(a))
+  p
