4ed4e6aabdece8c87239d1e4cf33378b6fd417da
RavichandranBE <37319294+RavichandranBE@users.noreply.github.com>
2018-05-24 14:02:46 +0530

    Update r.py

--- r.py
+++ r.py
@@ -1,14 +1,12 @@
-a=int(raw_input())
-b=int(raw_input())
- 
-for n in range(a,b):
+lower = int(input("Enter lower range: "))
+upper = int(input("Enter upper range: "))
+for num in range(lower, upper + 1):
+   order = len(str(num))
    sum = 0
-   temp = n
+   temp = num
    while temp > 0:
        digit = temp % 10
-       sum =sum+ digit ** 3
-       temp /= 10
- 
-   if n == sum:
-       print(n)
-       pr
+       sum += digit ** order
+       temp //= 10
+   if num == sum:
+       print(num)
