741b289831d23eba1fa9125ae8c432f0c67ff161
RavichandranBE <37319294+RavichandranBE@users.noreply.github.com>
2018-05-24 13:49:09 +0530

    Update ravi.py

--- ravi.py
+++ ravi.py
@@ -1,6 +1,7 @@
-a=int(input("enter a value of a")
-if(a<=100)
-print("yes")
-else
-print("no")
-pr
+n=int(input("Enter the number"))
+a=list(map(int,str(n)))
+b=list(map(lambda x:x**3,a))
+if(sum(b)==n):
+    print("The number is an armstrong number. ")
+else:
+    print("The number isn't an arsmtrong number. ")
