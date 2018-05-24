f98c5dc745a0c167228738671fd59b8c720046fd
RavichandranBE <37319294+RavichandranBE@users.noreply.github.com>
2018-05-24 14:13:34 +0530

    Update r2.py

--- r2.py
+++ r2.py
@@ -1,16 +1,8 @@
-def main():
-
- def multiple(m, n):
-
-   
-    a = range(n, (m * n)+1, n)
-
-    print (*a)
-
-
-    m = 5
-    n = input("")
-    multiple(m, n)
-if __name__ == '__main__':
-    main()
-    pr
+def multiple(a, b):
+ 
+    c = range(b, (a * b)+1, b)
+     
+    print(*c)
+a= int(input('enter the number a:'))
+b= int(input('enter the number b:'))
+multiple(a, b)
